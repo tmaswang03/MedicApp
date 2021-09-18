387aa799e26cda64f8a6d124d0b3f3b5ea52628d
tmaswang69 <60526513+tmaswang69@users.noreply.github.com>
2021-09-17 23:45:37 -0400

    Pushed face recognition stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  ]).then(start)
+
+function start() {
+    console.log("loaded")
+}
+
+
